2b47623ca228c7391a9ad8ce4278fa171c299f0c
Radhika Dangal <rdangal@pgh.yearup.org>
2023-05-09 11:37:28 -0400

    Added exercise

--- exercises/my_family.js
+++ exercises/my_family.js
@@ -1,5 +1,5 @@
 "use strict";
-const myFamily = ["Puspa", "Krishna", "Purna", "Prakash"];
+const myFamily = ["Puspa", "Krishna", "Ashish", "Purna", "Prakash", "Royce"];
 for (let i=0; i<myFamily.length; i++){
     console.log(myFamily[i]);
 }
